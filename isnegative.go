081e13932d7f807dfaff04f2adce0c4d51872526
nalmohse <noorjalmohsen@gmail.com>
2023-09-05 17:33:41 +0300

    negNumbers

---
+++ isnegative.go
@@ -0,0 +1,16 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func IsNegative(nb int) {
+
+	if nb < 0 {
+		z01.printRune('T')
+		z01.printRune('\n')
+	} else {
+		z01.printRune('F')
+		z01.printRune('\n')
+	}
+}
