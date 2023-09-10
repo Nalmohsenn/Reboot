356d60697b0ad04308fdbdc62ac636c46bbb52a7
nalmohse <noorjalmohsen@gmail.com>
2023-09-10 18:29:11 +0300

    764

---
+++ fibtest/fibtest.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+func main() {
+	arg1 := 4
+	fmt.Println(piscine.Fibonacci(arg1))
+}
---
+++ firstrune.go
@@ -0,0 +1,6 @@
+package piscine
+
+func FirstRune(s string) rune {
+	array := []Rune(s)
+	return arr[len(s)-1]
+}
