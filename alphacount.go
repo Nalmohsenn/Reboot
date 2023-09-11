4c1c143fd4cf0e9a8b78c3fcf6c76358f6bb45b6
nalmohse <noorjalmohsen@gmail.com>
2023-09-11 18:18:42 +0300

    countrunee

--- alphacount.go
+++ alphacount.go
@@ -1,15 +1,10 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
-
 func AlphaCount(s string) int {
 	count := 0
 	arr := []rune(s)
-
 	for i := 0; i < len(s); i++ {
-		if (arr[i] >= 'A' && arr <= 'Z') || (arr[i] >= 'a' && arr <= 'z') {
+		if (arr[i] >= 'A' && arr[i] <= 'Z') || (arr[i] >= 'a' && arr[i] <= 'z') {
 			count++
 		}
 	}
