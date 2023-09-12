57336a16c286dc0133828769559754527c2e4feb
nalmohse <noorjalmohsen@gmail.com>
2023-09-12 16:02:52 +0300

    index

---
+++ index.go
@@ -0,0 +1,45 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	index := -1
+	index2 := 0
+	count := 0
+	if len(s) > 0 && len(toFind) > 0 {
+		if len(s) == len(toFind) {
+			for i := 0; i <= len(s); i++ {
+				if compare(s, toFind) == 0 {
+					index = i
+					return index
+				}
+			}
+		} else if len(s) > len(toFind) {
+			for i := 0; i < len(s); i++ {
+				if s[i] == toFind[0] {
+					index = i
+					break
+				}
+			}
+			if len(toFind) > 1 && index != -1 {
+				for i := index; i < len(s); i++ {
+					if s[i] == toFind[index2] {
+						count++
+						index2++
+						if count == len(toFind) {
+							return index
+						}
+					} else {
+						break
+					}
+				}
+				if count == len(toFind) {
+					return index
+				}
+			} else {
+				return index
+			}
+		}
+	} else {
+		return -1
+	}
+	return index
+}
