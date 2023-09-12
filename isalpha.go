28f1af6fac43f70133dd8eb96ef99ccdfd144f76
nalmohse <noorjalmohsen@gmail.com>
2023-09-12 17:58:30 +0300

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -4,7 +4,7 @@ func IsAlpha(s string) bool {
 	arr := []rune(s)
 	count := 0
 	for i := 0; i < len(s); i++ {
-		if (arr[i] >= 'A' && arr[i] <= 'Z') || (arr[i] >= 'a' && arr[i] <= 'z') || (arr[i] >= '0' && arr[i] <= '9') {
+		if (arr[i] >= '0' && arr[i] <= '9') || (arr[i] >= 'a' && arr[i] <= 'z') || (arr[i] >= 'A' && arr[i] <= 'Z') {
 			count++
 		}
 	}
