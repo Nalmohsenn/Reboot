4e43a09f965762d47e7951843e8fbd348f919d1c
nalmohse <noorjalmohsen@gmail.com>
2023-09-12 17:21:25 +0300

    Islower

--- islower.go
+++ islower.go
@@ -3,7 +3,7 @@ package piscine
 func IsLower(s string) bool {
 	arr := []rune(s)
 	for i := 0; i < len(s); i++ {
-		if arr[i] >= 'A' || arr[i] <= 'Z' {
+		if arr[i] < 'a' || arr[i] > 'z' {
 			return false
 		}
 	}
