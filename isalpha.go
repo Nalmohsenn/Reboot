71b6e1fbe1f3f8519a29288e93471df70f72ea54
nalmohse <noorjalmohsen@gmail.com>
2023-09-12 17:51:39 +0300

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -1,11 +1,16 @@
 package piscine
 
-func IsLower(s string) bool {
+func IsAlpha(s string) bool {
 	arr := []rune(s)
+	count := 0
 	for i := 0; i < len(s); i++ {
-		if arr[i] == ' ' {
-			return false
+		if (arr[i] >= 'A' && arr[i] <= 'Z') || (arr[i] >= 'a' && arr[i] <= 'z') || (arr[i] >= '0' && arr[i] <= '9') {
+			count++
 		}
 	}
-	return true
+	if count == len(s) {
+		return true
+	} else {
+		return false
+	}
 }
