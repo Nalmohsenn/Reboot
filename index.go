4d6f97c1d964d37793e240f754f427256caf3c6c
noor jafar <nalmohse@desk-0009.reboot.internal>
2023-09-12 15:27:36 +0300

    indexx

---
+++ index.go
@@ -0,0 +1,14 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	index := -1
+	index2 := 0
+	count := 0
+	if len(s) > 0 && len(toFind) > 0 {
+		for i := 0; i <= len(s); i++ {
+
+		}
+
+	}
+
+}
