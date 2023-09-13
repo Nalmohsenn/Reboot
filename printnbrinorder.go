bb12e1aa7ea7ae5f7c9820e014c1c79560d1f9b7
nalmohse <noorjalmohsen@gmail.com>
2023-09-13 15:38:51 +0300

    printnb

---
+++ printnbrinorder.go
@@ -0,0 +1,41 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintNbrInOrder(n int) {
+	nb := n
+	count := true
+	arr := [19]int{}
+	countnb := 0
+	if nb == 0 {
+		z01.PrintRune(rune(nb + 48))
+	} else {
+		for i := 0; count != false; i++ {
+			if nb != 0 {
+				arr[i] = nb % 10
+				nb = nb / 10
+				countnb++
+			} else {
+				countnb = false
+			}
+		}
+		temp := 0
+		for j := 0; j < countnb; j++ {
+			for v := v + 1; v < countnb; v++ {
+				if arr[j] > arr[v] {
+					temp = arr[j]
+					arr[j] = arr[v]
+					arr[v] = temp
+				}
+			}
+		}
+		for _, t := range arr {
+			if countnb > 0 {
+				z01.PrintRune(rune(t + 48))
+				countnb--
+			}
+		}
+	}
+}
