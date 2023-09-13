b483771df7860549a3a0a1a7a0e31efccaaa6ff8
nalmohse <noorjalmohsen@gmail.com>
2023-09-13 17:11:39 +0300

    printprogramm

---
+++ printprogramname/main.go
@@ -0,0 +1,18 @@
+package piscine
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main(){
+    arguments := os.Args 
+	arr := [rune(arguments[0])]
+	for i := 0; i < len(arr); i++ {
+		if arr[i] != '.' && array[i] != '/'{
+			z01.PrintRune(arr[i])
+		}
+	}
+    z01.PrintRune('\n')
+}
